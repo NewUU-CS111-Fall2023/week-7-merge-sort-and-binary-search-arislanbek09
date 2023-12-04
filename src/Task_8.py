7f60e3c3cc706ae9cd984e0b43c08b42e6a6f1fe
Arislanbek Kalbaev <127713555+arislanbek09@users.noreply.github.com>
2023-12-04 21:38:45 +0500

    Create Task_8.py

---
+++ src/Task_8.py
@@ -0,0 +1,11 @@
+def recursivePow(x, n):
+    if n == 0:
+        return 1
+    else:
+        return x * recursivePow(x, n - 1)
+
+result = recursivePow(12, 2)
+
+print(result)
+
+#O(n) where n = number of items in the list
